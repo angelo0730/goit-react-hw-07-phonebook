646bece74b7ca1e8d79c8cb227f32127c62882fb
angelo0730 <137179844+angelo0730@users.noreply.github.com>
2024-05-27 12:46:34 +0800

    update app js

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,13 @@
+// App.js
 import { ContactForm } from './ContactForm/ContactForm';
 import { Filter } from './Filter/Filter';
 import { ContactList } from './ContactList/ContactList';
+import './App.css';
 
 export const App = () => {
   return (
-    <div>
-      <h1>Phonebook</h1>
+    <div className="container">
+      <h1>Phonebook📱</h1>
       <ContactForm />
       <h2>Contacts</h2>
       <Filter />
